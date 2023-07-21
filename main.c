615df16dca0dfc1d6a9e837751f868441cf9dcc2
r0ckinxj3 <39844689+r0ckinxj3@users.noreply.github.com>
2023-07-21 12:11:27 +0100

    Add files via upload

---
+++ main.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+
+int main(){
+
+  char op;
+  int first,second;
+  printf("Enter an operator (+, -, *, /): ");
+  scanf("%c",&op);
+  printf("Enter two operands: ");
+  scanf("%d %d",&first,&second);
+  switch(op) {
+
+   case '+':
+      printf("%d + %d = %d", first, second, first + second);
+      break;
+   case '-':
+      printf("%d - %d = %d", first, second, first - second);
+      break;
+    case '*':
+      printf("%d * %d = %d", first, second, first * second);
+      break;
+    case '/':
+      printf("%d / %d = %d", first, second, first / second);
+      break;
+    // operator doesn't match any case constant
+    default:
+      printf("Error! operator is not correct");
+  }
+  return 0;
+
+
+}
